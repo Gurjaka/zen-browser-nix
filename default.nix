ca159fce03d134ad86ef2e90a0f949c1d3f7cbfa
Sigmanificient <edhyjox@gmail.com>
2024-12-28 18:09:40 +0100

    Refactor the derivation

--- default.nix
+++ default.nix
@@ -51,14 +51,14 @@
   wrapGAppsHook,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "zen-browser";
   version = "1.0.2-b.5";
 
   src = fetchFromGitHub {
     owner = "zen-browser";
     repo = "desktop";
-    rev = "refs/tags/v${version}";
+    rev = "refs/tags/v${finalAttrs.version}";
     hash = "sha256:1xp0z86l7z661cwckgr623gwwjsy3h66900xqjq6dvgx5a3njbxi";
   };
 
@@ -124,39 +124,55 @@ stdenv.mkDerivation rec {
 
     install -D $desktopSrc/zen.desktop $out/share/applications/zen.desktop
 
-    install -D $src/browser/chrome/icons/default/default16.png $out/share/icons/hicolor/16x16/apps/zen.png
-    install -D $src/browser/chrome/icons/default/default32.png $out/share/icons/hicolor/32x32/apps/zen.png
-    install -D $src/browser/chrome/icons/default/default48.png $out/share/icons/hicolor/48x48/apps/zen.png
-    install -D $src/browser/chrome/icons/default/default64.png $out/share/icons/hicolor/64x64/apps/zen.png
-    install -D $src/browser/chrome/icons/default/default128.png $out/share/icons/hicolor/128x128/apps/zen.png
+    for i in 16 32 48 64 128; do
+        install -Dm 644 $src/browser/chrome/icons/default/default$i.png \
+          $out/share/icons/hicolor/$ix$i/apps/zen.png
+    done
   '';
 
-  fixupPhase = ''
-    chmod 755 $out/bin/zen $out/opt/zen/*
+  fixupPhase =
+    let
+      ld-lib-path = lib.makeLibraryPath finalAttrs.runtimeLibs;
+    in
+    ''
+      chmod 755 $out/bin/zen $out/opt/zen/*
+      interpreter=$(cat $NIX_CC/nix-support/dynamic-linker)
 
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/zen
-    wrapProgram $out/opt/zen/zen --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}" \
-                         --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
+      patchelf --set-interpreter "$interpreter" $out/opt/zen/zen
+      wrapProgram $out/opt/zen/zen \
+          --set LD_LIBRARY_PATH "${ld-lib-path}" \
+          --set MOZ_LEGACY_PROFILES 1 \
+          --set MOZ_ALLOW_DOWNGRADE 1 \
+          --set MOZ_APP_LAUNCHER zen \
+          --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
 
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/zen-bin
-    wrapProgram $out/opt/zen/zen-bin --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}" \
-                         --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
+      patchelf --set-interpreter "$interpreter" $out/opt/zen/zen-bin
+      wrapProgram $out/opt/zen/zen-bin \
+        --set LD_LIBRARY_PATH "${ld-lib-path}" \
+        --set MOZ_LEGACY_PROFILES 1 \
+        --set MOZ_ALLOW_DOWNGRADE 1 \
+        --set MOZ_APP_LAUNCHER zen \
+        --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
 
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/glxtest
-    wrapProgram $out/opt/zen/glxtest --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}"
+      patchelf --set-interpreter "$interpreter" $out/opt/zen/glxtest
+      wrapProgram $out/opt/zen/glxtest \
+          --set LD_LIBRARY_PATH "${ld-lib-path}"
 
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/updater
-    wrapProgram $out/opt/zen/updater --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}"
+      patchelf --set-interpreter "$interpreter" $out/opt/zen/updater
+      wrapProgram $out/opt/zen/updater \
+          --set LD_LIBRARY_PATH "${ld-lib-path}"
 
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/vaapitest
-    wrapProgram $out/opt/zen/vaapitest --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}"
-  '';
+      patchelf --set-interpreter "$interpreter" $out/opt/zen/vaapitest
+      wrapProgram $out/opt/zen/vaapitest \
+          --set LD_LIBRARY_PATH "${ld-lib-path}"
+    '';
 
   meta = {
-    changelog = "https://zen-browser.app/release-notes/#${version}";
+    changelog = "https://zen-browser.app/release-notes/#${finalAttrs.version}";
     homepage = "https://zen-browser.app/";
-    description = "Experience tranquillity while browsing the web without people tracking you! ";
+    description = "Experience tranquillity while browsing the web without people tracking you";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ gurjaka ];
+    mainProgram = "zen";
   };
-}
+})
