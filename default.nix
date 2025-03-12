5ec64ea1f6f546caa0ee5c2c3b3a722fe87330cf
Gurjaka <esartia.gurika@gmail.com>
2025-03-13 00:28:46 +0400

    fix icons

--- default.nix
+++ default.nix
@@ -132,26 +132,26 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   installPhase = ''
-    mkdir -p $out/{bin,opt/zen} && cp -r $src/* $out/opt/zen
-    ln -s $out/opt/zen/zen $out/bin/zen
+    mkdir -p $out/{bin,opt} && cp -r $src/* $out/opt/
+    ln -s $out/opt $out/bin/zen
 
     install -D $desktopSrc/zen.desktop $out/share/applications/zen.desktop
 
     for i in 16 32 48 64 128; do
-        install -Dm 644 $src/browser/chrome/icons/default/default$i.png \
-          $out/share/icons/hicolor/$ix$i/apps/zen.png
+        install -D $src/browser/chrome/icons/default/default$i.png \
+          $out/share/icons/hicolor/''${i}x''${i}/apps/zen.png
     done
   '';
 
   fixupPhase = let
     ld-lib-path = lib.makeLibraryPath finalAttrs.runtimeLibs;
   in ''
-    chmod 755 $out/bin/zen $out/opt/zen/*
+    chmod 755 $out/bin/* $out/opt/*
     interpreter=$(cat $NIX_CC/nix-support/dynamic-linker)
 
     for bin in zen zen-bin; do
-       patchelf --set-interpreter "$interpreter" $out/opt/zen/$bin
-       wrapProgram $out/opt/zen/$bin \
+       patchelf --set-interpreter "$interpreter" $out/opt/$bin
+       wrapProgram $out/opt/$bin \
            --set LD_LIBRARY_PATH "${ld-lib-path}" \
            --set MOZ_LEGACY_PROFILES 1 \
            --set MOZ_ALLOW_DOWNGRADE 1 \
@@ -160,8 +160,8 @@ stdenv.mkDerivation (finalAttrs: {
     done;
 
     for bin in glxtest updater vaapitest; do
-        patchelf --set-interpreter "$interpreter" $out/opt/zen/$bin
-        wrapProgram $out/opt/zen/$bin \
+        patchelf --set-interpreter "$interpreter" $out/opt/$bin
+        wrapProgram $out/opt/$bin \
             --set LD_LIBRARY_PATH "${ld-lib-path}"
     done;
   '';
